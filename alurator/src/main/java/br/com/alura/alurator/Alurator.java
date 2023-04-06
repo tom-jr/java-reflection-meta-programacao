ec1daa13eaae296f593584c543cba4ed40607365
tom-jr <antonio.carlos@postoaki.com.br>
2023-04-06 05:42:04 -0300

    # configs

--- alurator/src/main/java/br/com/alura/alurator/Alurator.java
+++ alurator/src/main/java/br/com/alura/alurator/Alurator.java
@@ -1,10 +1,30 @@
 package br.com.alura.alurator;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Alurator {
-	public Object executa(String url) {
-		// TODO - processa a requisicao executando o metodo
-		// da classe em questao
-		
-		return null;
-	}
+    private final String qualifyName;
+
+    public Alurator(String qualifyName) {
+        this.qualifyName = qualifyName;
+    }
+
+    public Object executa(String url) {
+
+        String[] urlFragmentos = url.replaceFirst("/", "").split("/");
+
+        try {
+            Class<?> myClass = Class.forName(String.format("%s.%s%s", this.qualifyName,
+                    StringUtils.capitalize(urlFragmentos[0]),"Controller"));
+
+            Object object = myClass.newInstance();
+            System.out.println(object);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+
+        return null;
+    }
 }
